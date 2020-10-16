25f188a85b289a594c5a4b01ac6bd8639f7049c5
TJFitz <tjfitz@comcast.net>
2020-10-16 11:05:58 -0400

    minor bugfixes/heroku deployment needed otherwise complete

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,15 +7,12 @@ const FILES_TO_CACHE = [
   "./icons/icon-192x192.png",
   "./icons/icon-512x512.png",
   "/index.js",
+  "/db.js",
+  "/manifest.webmanifest",
 ];
 
 // install
 self.addEventListener("install", function (evt) {
-  // // pre cache image data
-  // evt.waitUntil(
-  //   caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
-  // );
-
   // pre cache all static assets
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
